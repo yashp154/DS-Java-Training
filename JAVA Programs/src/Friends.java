353ad8fe494634245e6768bf24dd75a9188bbcf9
Yash Pandey <yash.154.p@gmail.com>
2024-07-24 12:21:27 +0530

    Print your friends through with array

---
+++ JAVA Programs/src/Friends.java
@@ -0,0 +1,10 @@
+public class Friends {
+   public static void main(String[] args){
+        String name[] ={ "Adarsh","Aman","Anshu","Vardhan","Harsh"};
+        name[3]="Vishesh";
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(name[i]);
+        }
+    }
+}
