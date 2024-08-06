286c79231b7dad88242d6723a1f22fb537f75924
Yash Pandey <yash.154.p@gmail.com>
2024-08-06 12:10:05 +0530

    Learning Wrapper Class

---
+++ Java Projects/src/OOPs/WrapperClasses.java
@@ -0,0 +1,11 @@
+package OOPs;
+
+public class WrapperClasses {
+    public static void main(String args[])
+    {
+        int a = 3;
+        Integer b = 5;
+        String c = String.valueOf(a) + b.toString();
+        System.out.println(c);
+    }
+}
